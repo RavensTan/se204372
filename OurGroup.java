2cd3ab2c3fffa221064ee7ff2049ff42b7181d83
RavensT <91468726+RavensTan@users.noreply.github.com>
2022-01-26 15:10:13 +0700

    edit1_Thanrak

--- OurGroup.java
+++ OurGroup.java
@@ -6,7 +6,7 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
-        groupMembers.add("YOUR NAME"); //PUT YOUR NAME HERE
+        groupMembers.add("แทนรัก");
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
