c95e2a917e1e011d718a1e3af06ba314202d9515
RavensT <91468726+RavensTan@users.noreply.github.com>
2022-01-26 15:48:06 +0700

    add student_id

--- OurGroup.java
+++ OurGroup.java
@@ -6,11 +6,11 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
-        groupMembers.add("แทนรัก");
-        groupMembers.add("วุฒิตุลย์");
-        groupMembers.add("สิทธิพัฒน์");
-        groupMembers.add("ฐิชฌาภรณ์");
-        groupMembers.add("วรัญญา");
+        groupMembers.add("แทนรัก 6120502688 หัวหน้ากลุ่ม");
+        groupMembers.add("วุฒิตุลย์ 6120502769");
+        groupMembers.add("สิทธิพัฒน์ 6120501771");
+        groupMembers.add("ฐิชฌาภรณ์ 6120501673");
+        groupMembers.add("วรัญญา 6120504001");
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
