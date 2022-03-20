5b324943a74fcd49fbc773135d03abff90616ec4
6120501771 <98451245+6120501771@users.noreply.github.com>
2022-03-20 18:09:50 +0700

    Big

---
+++ สถานประกอบการ.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>สถานประกอบการ</title>
+</head>
+<body>
+
+</div>
+            <div class="col-md-8">
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2  mb-md-0">
+                    <li><a href="index.php" class="nav-link px-2 text">หน้าหลัก</a></li>
+                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text">บัญชีผู้ใช้</a></li>
+                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text">เอกสารที่เกี่ยวข้อง</a></li>
+                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text">จัดการเอกสาร</a></li>
+                    <li><a href="สถานประกอบการ.php" class="nav-link px-2 text">สถานประกอบการ</a></li>
+                    <li><a href="ประวัติการฝึกงาน.php" class="nav-link px-2 text">ประวัติการฝึกงาน</a></li>
+                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text">ออกจากระบบ</a></li>
+            </ul>
+
+
+
+
+                </ul>
+            </div>
+
+    <div class="header">
+        <h2>
+            <p align=center>สถานประกอบการ</p>
+        <h2>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br>    
+   <h3><p align=center>เอกสารรายชื่อสถานประกอบการ</p><h3>
+       <p align=center>	<button><a href= "https://drive.google.com/drive/folders/1qGsgDdgsuGCLcV7fMO3uljl21b_47XUd?usp=sharing"> Download file </a></button></p>
+
+ 
+  
+
+   
+   
+</body>
+</html>
