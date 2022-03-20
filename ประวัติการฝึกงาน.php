e98f406b845ab2e39ca743958151f33d0a7b689e
6120501771 <98451245+6120501771@users.noreply.github.com>
2022-03-20 19:13:02 +0700

    BIG2

--- ประวัติการฝึกงาน.php
+++ ประวัติการฝึกงาน.php
@@ -1,37 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<title>ประวัติการฝึกงาน</title>
+<html>
+<header class="p-3 bg-dark text-white">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ประวัติการฝึกงาน</title>
-</head>
-<body>
-</div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 d-flex flex-wrap align-items-center justify-content-lg-start">
+                <h3>ระบบจัดการการฝึกงานและสหกิจแบบออนไลน์</h3>
+
+               
+            </div>
             <div class="col-md-8">
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2  mb-md-0">
-                    <li><a href="index.php" class="nav-link px-2 text">หน้าหลัก</a></li>
-                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text">บัญชีผู้ใช้</a></li>
-                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text">เอกสารที่เกี่ยวข้อง</a></li>
-                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text">จัดการเอกสาร</a></li>
-                    <li><a href="สถานประกอบการ.php" class="nav-link px-2 text">สถานประกอบการ</a></li>
-                    <li><a href="ประวัติการฝึกงาน.php" class="nav-link px-2 text">ประวัติการฝึกงาน</a></li>
-                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text">ออกจากระบบ</a></li>
-            </ul>
-
-
-
+                    <li><a href="?controller=pages&action=home" class="nav-link px-2 text-white">หน้าหลัก</a></li>
+                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text-white">บัญชีผู้ใช้</a></li>
+                    <li><a href="?controller=relatedDocument&action=relatedDocument" class="nav-link px-2 text-white">เอกสารที่เกี่ยวข้อง</a></li>
+                    <li><a href="?controller=manage&action=manage_student_list" class="nav-link px-2 text-white">จัดการเอกสาร</a></li>
+                    <li><a href="สถานประกอบการ.php" class="nav-link px-2 text-white">สถานประกอบการ</a></li>
+                    <li><a href="ประวัติการฝึกงาน.php" class="nav-link px-2 text-white">ประวัติการฝึกงาน</a></li>
+                    <li><a href="?controller=user&action=user_list" class="nav-link px-2 text-white">ออกจากระบบ</a></li>
+                    <!-- <li><a href="?controller=petition&action=petition_list" class="nav-link px-2 text-white">petition</a></li>
+                <li><a href="?controller=petition&action=PetAdmin_list" class="nav-link px-2 text-white">petition_ad</a></li> -->
+                    <!-- <li><a href="?controller=quataion&action=index" class="nav-link px-2 text-white">Quatation</a></li>
+                <li><a href="?controller=quationdetail&action=index" class="nav-link px-2 text-white">QuatationDetail</a></li>
+                <li><a href="?controller=ProductDetail&action=index" class="nav-link px-2 text-white">ProductDetail</a></li> -->
 
                 </ul>
             </div>
-   <div class="header">
-   <h2><p align=center>ประวัติการฝึกงาน</p><h2>
-   </div>
-   <br><br><br><br><br><br><br><br><br><br>
-   <h3><p align=center>ไฟล์ประวัติการฝึกงานของนิสิต</p><h3>
-   <p align=center>	<button><a href= "https://drive.google.com/drive/folders/1qGsgDdgsuGCLcV7fMO3uljl21b_47XUd?usp=sharing"> Download file </a></button></p>
-   
-   
+        </div>
+
+    </div>
+</header>
+</head>
+<body style="background-color:#c1ffa7;">
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <h1><p align=center>ประวัติการฝึกงาน</p></h1><br>
+    <p align=center>	<button><a href= "https://drive.google.com/drive/folders/1qGsgDdgsuGCLcV7fMO3uljl21b_47XUd?usp=sharing"> Download file </a></button></p>
+
 </body>
 </html>
+
