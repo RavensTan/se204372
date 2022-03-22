2253f51fc005ef147ab3184dc90541aeb0d10736
wuttitul <91468323+wuttitul@users.noreply.github.com>
2022-03-22 22:25:11 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,6 +5,7 @@
     $dbname = "se_project";
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
+    mysqli_set_charset($conn, "utf8");
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
