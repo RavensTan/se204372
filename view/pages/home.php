07ce93fd87e2b4f6229e497b35f0c606b9d57bb7
wuttitul <91468323+wuttitul@users.noreply.github.com>
2022-03-23 01:05:09 +0700

    Update home.php

--- view/pages/home.php
+++ view/pages/home.php
@@ -1,3 +1,52 @@
-<p>
-    Homepage
-</p>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page Title</title>
+
+    <style>
+    {
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        background-image: url("image/bg3.png");
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+        height: 100vh;
+    }
+
+    div.transbox {
+  margin: 60px;
+  background-color: #ffffff;
+  border: 1px solid black;
+  opacity: 0.9;
+}
+
+div.transbox p {
+  margin: 15%;
+  font-weight: bold;
+  color: #000000;
+}
+    </style>
+
+</head>
+
+<body>
+
+<div class="background">
+  <div class="transbox"><br><br>
+    <center><h1>ข่าวสาร</h1>
+
+
+    <img src="image/img1.jpg" alt="message" style="width:600px;height:800px;"></center>
+  </div>
+
+</div>
+
+
+</body>
+
+</html>
